5ce7d2fb7f6d24187be80683f4cb9336f548bfbc
HUGE\ddii <daniel.teixeira@hcuge.ch>
2022-03-31 14:26:11 +0200

    adding scenarios

--- js/impl/praise.js
+++ js/impl/praise.js
@@ -9,21 +9,7 @@ function praise_implementation(parameters, positive_hemos) {
     var date = moment(date_str);
 
     return {
-        episodes: [{
-            "patient_id": "patient_test",
-            "stay_id": "stay_test",
-            "labo_sample_date": date.format("YYYY-MM-DD"),
-            "labo_germ_name": "pathA",
-            "labo_commensal": "0",
-            "labo_sample_datetime_moment": date,
-            "labo_sample_datetime_timestamp": date.valueOf(),
-            "labo_polymicrobial_germs": [
-                "pathA",
-                "pathB"
-            ],
-            "labo_polymicrobial_count": 1,
-            "evidences_count": 1
-        }]
+        episodes: []
     }
 
 }
