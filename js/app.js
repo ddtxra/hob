19fbbddae35e0647bbf7119eba16efc5865673b9
ddtxra <ddtxra@gmail.com>
2022-09-24 01:33:39 +0200

    adding cases

--- js/app.js
+++ js/app.js
@@ -1,53 +1,25 @@
 $.getJSON("static/cases.json", function(cases) {
-    console.log(cases)
-    var cases = _.groupBy(cases, "patient_id");
-    showCase(cases["patient_1019"]);
+    showCases(cases);
 });
 
-
-function showCase(cas) {
-
-    var positive_hemocultures = cas.filter(c => c.description == "");
-    var algo = $('#algo-selector').find(":selected").val();
-    var parameters = {
-        implementation: algo
-    };
+function showCases(cases) {
 
     $('#description').html("");
     $("#fv_pos_hemo").empty();
     $("#fv_episodes").empty();
     $("#fv_expected").empty();
-    $('#pos_hemo_grid').empty();
-    $('#episodes_grid').empty();
+    var algo = $('#algo-selector').find(":selected").val();
 
+    cases.filter(c => c.description != "").forEach(function(c) {
+        var positive_hemocultures = cases.filter(p => ((c.patient_id == p.patient_id) && (p.description == "")));
+        showCase(c.patient_id, c.description, positive_hemocultures)
+    })
+
+}
+
+function showCase(patient_id, description, positive_hemocultures) {
     //var pos_hemo_txt = convertJSONForTxtArea(positive_hemocultures);
     //$('#pos_hemocultre_txtarea').val(pos_hemo_txt);
-    $('#description').html(cas.filter(c => c.description != "")[0].description);
-
-    var result = computeBSIEpisodes(parameters, positive_hemocultures);
-
-    updateVis(positive_hemocultures, result.episodes);
+    $('#description').html(description);
+    updateVis(patient_id, positive_hemocultures);
 }
-
-
-function showRawTab() {
-    $("#rawDataTab").addClass("active");
-    $("#computedDataTab").removeClass("active");
-    $("#pos_hemo_grid").show();
-    $("#episodes_grid").hide();
-}
-
-function showComputedTab() {
-    $("#computedDataTab").addClass("active");
-    $("#rawDataTab").removeClass("active");
-    $("#pos_hemo_grid").hide();
-    $("#episodes_grid").show();
-}
-
-$('#rawDataTab').click(function() {
-    showRawTab();
-});
-
-$('#computedDataTab').click(function() {
-    showComputedTab();
-});
--- js/vis.js
+++ js/vis.js
@@ -1,6 +1,24 @@
-function updateVis(positive_hemos, episodes, expected) {
+function updateVis(patient_id, positive_hemos, expected) {
 
+
+    var episodes_hug_v0 = computeBSIEpisodes({
+        implementation: "HUG" //HUGV0
+    }, positive_hemos)["episodes"];
+
+
+    var episodes_hug_v2 = computeBSIEpisodes({
+        implementation: "HUGV2" //HUGV2
+    }, positive_hemos)["episodes"];
+
+    var episodes_praise = computeBSIEpisodes({
+        implementation: "HUGV2" //PRAISE
+    }, positive_hemos)["episodes"];
+
+
+    console.log(episodes_hug_v0);
+    
     var positive_hemocultures = prepareData(deepCopy((positive_hemos)));
+
 
     // let the grid know which columns and what data to use
     var fvParams = {
@@ -12,15 +30,13 @@ function updateVis(positive_hemos, episodes, expected) {
         zoomMax: 1 //define the maximum range of the zoom
     };
 
-
-    function addFeature(ft, series, epi) {
-        var lines = _.groupBy(series, epi ? "patient_id" : function(p) { return p.patient_id + "@" + p.stay_id });
+    function addPositiveHemocultures(ft, positive_hemocultures) {
+        var lines = _.groupBy(positive_hemocultures, function(p) { return p.patient_id + "@" + p.stay_id });
         Object.keys(lines).sort().forEach(pat => {
-
             var pos_hem = lines[pat];
-            var feature = pos_hem.map(function(ph) {
+            var feature_pos_hemo = pos_hem.map(function(ph) {
                 var day_of_year = ph.labo_sample_datetime_moment.dayOfYear();
-                var label = ((epi && ph.labo_polymicrobial_germs) ? ph.labo_polymicrobial_germs.join("+") : ph.labo_germ_name);
+                var label = ((false && ph.labo_polymicrobial_germs) ? ph.labo_polymicrobial_germs.join("+") : ph.labo_germ_name);
                 return {
                     x: day_of_year,
                     y: day_of_year + 0.999,
@@ -28,49 +44,60 @@ function updateVis(positive_hemos, episodes, expected) {
                     color: getColorFromPalette(label)
                 }
             });
-
+    
             ft.addFeature({
-                data: feature,
+                data: feature_pos_hemo,
                 name: pat,
                 className: "test",
                 type: "rect" // ['rect', 'path', 'line']
             });
+    
+        });
+       
+    }
+
+    function addEpisodes(ft, episodes, name){
+
+        var feature_episodes = episodes.map(function(ph) {
+            var day_of_year = ph.labo_sample_datetime_moment.dayOfYear();
+            var label = ((true && ph.labo_polymicrobial_germs) ? ph.labo_polymicrobial_germs.join("+") : ph.labo_germ_name);
+            return {
+                x: day_of_year,
+                y: day_of_year + 0.999,
+                description: label,
+                color: getColorFromPalette(label)
+            }
+        });
+
+        ft.addFeature({
+            data: feature_episodes,
+            name: name,
+            className: "test",
+            type: "rect" // ['rect', 'path', 'line']
         });
     }
 
 
-    const gridDiv = document.querySelector('#pos_hemo_grid');
-    const columnDefsForPosHemos = Object.keys(positive_hemos[0]).map(function(k) { return { field: k, width: 200 } });
-
-    new agGrid.Grid(gridDiv, {
-        columnDefs: columnDefsForPosHemos,
-        rowData: positive_hemos
-    });
 
     var timestamps = (positive_hemocultures.map(p => p.labo_sample_datetime_timestamp));
     var fv_length = Math.round((Math.max(...timestamps) - Math.min(...timestamps)) / 1000 / 60 / 60 / 24) + 7;
 
-    const columnDefsForEpisodes = Object.keys(episodes[0]).map(function(k) { return { field: k, width: 200 } }).filter(f => !f.field.endsWith("moment") && !f.field.endsWith("timestamp"));
-    const episodes_grid = document.querySelector('#episodes_grid');
+    var div_id =  "fv_pos_hemo_" + patient_id;
 
-    new agGrid.Grid(episodes_grid, {
-        columnDefs: columnDefsForEpisodes,
-        rowData: episodes
-    });
+    $('#fv_pos_hemo').append($('<span>' + patient_id + '</span><div style="margin-top: -35px;" id=' + div_id+ '></div>'));
+    var feat_v = new FeatureViewer.createFeature(fv_length, "#" + div_id, fvParams);
 
-
-    addFeature(new FeatureViewer.createFeature(fv_length, "#fv_pos_hemo", fvParams), positive_hemocultures);
-    addFeature(new FeatureViewer.createFeature(fv_length, "#fv_episodes", fvParams), episodes, true);
-    addFeature(new FeatureViewer.createFeature(fv_length, "#fv_classification", fvParams), episodes, true);
+    addPositiveHemocultures(feat_v, positive_hemocultures);
+    addEpisodes(feat_v, episodes_hug_v0, "episodes HUG_v0");
+    addEpisodes(feat_v, episodes_hug_v2, "episodes HUG_v2");
+    addEpisodes(feat_v, episodes_praise, "episodes praise");
+    
+    //addFeature(new FeatureViewer.createFeature(fv_length, "#fv_classification", fvParams), episodes, true);
 
     /*if (expected && expected.data && expected.data.length > 0) {
         addFeature(new FeatureViewer.createFeature(fv_length, "#fv_expected", fvParams), prepareData(expected.data), true);
     }*/
 
-    showRawTab();
-
-    $("#rawDataTab").text("Raw data (" + positive_hemocultures.length + " positive hemocultures)");
-    $("#computedDataTab").text("Computed episodes (" + episodes.length + " episodes)");
     if (expected && expected.description) {
         $("#explanation_expected").html(expected.description);
     } else {
